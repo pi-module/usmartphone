d77abb609f53383dad458eee97267d711da59cac
voltan <djvoltan@gmail.com>
2014-10-21 20:33:51 +0330

    Update module

--- src/Controller/Front/IndexController.php
+++ src/Controller/Front/IndexController.php
@@ -19,8 +19,22 @@ use Zend\Json\Json;
 
 class IndexController extends ActionController
 {
+    public function indexAction()
+    {
+        // Set return array
+        $return = array(
+            'active'    => $this->config('active_login'),
+            'message'   => '',
+        );
+        // json output
+        $this->view()->setTemplate(false)->setLayout('layout-content');
+        echo Json::encode($return);
+        exit;
+    }
+
     public function loginAction()
     {
+        // Set return array
         $return = array(
             'message'   => '',
             'login'     => 0,
@@ -28,43 +42,73 @@ class IndexController extends ActionController
             'userid'    => 0,
             'sessionid' => '',
         );
-            
-        if (!$this->request->isPost()) {
-            $return['message'] = __('Invalid input please try again');
-        } else {
-            $post = $this->request->getPost();
-            $identity = $post['identity'];
-            $credential = $post['credential'];
-            $result = Pi::service('authentication')->authenticate($identity, $credential);
-            $result = $this->verifyResult($result);
-            if ($result->isValid()) {
-                $uid = (int) $result->getData('id');
-                $user = Pi::model('user_account')->find($uid)->toArray();
-                // Set return array
-                $return['message']    = __('You have logged in successfully');
-                $return['login']      = 1;
-                $return['identity']   = $user['identity'];
-                $return['userid']     = $user['id'];
-                $return['sessionid']  = Pi::service('session')->getId();
-            } else {
+        // Check user login from allowed or not
+        if ($this->config('active_login')) {
+            // Check post array set or not
+            if (!$this->request->isPost()) {
                 $return['message'] = __('Invalid input please try again');
+            } else {
+                // Get from post
+                $post = $this->request->getPost();
+                $identity = $post['identity'];
+                $credential = $post['credential'];
+                // Try login
+                $result = Pi::service('authentication')->authenticate($identity, $credential);
+                $result = $this->verifyResult($result);
+                // Check login is valid
+                if ($result->isValid()) {
+                    $uid = (int) $result->getData('id');
+                    // Bind user information
+                    if (Pi::service('user')->bind($uid)) {
+                        Pi::service('session')->setUser($uid);
+                        $rememberMe = 14 * 86400;
+                        Pi::service('session')->manager()->rememberme($rememberMe);
+                        // Unset login session
+                        if (isset($_SESSION['PI_LOGIN'])) {
+                            unset($_SESSION['PI_LOGIN']);
+                        }
+                        // Set user login event
+                        $args = array(
+                            'uid'           => $uid,
+                            'remember_time' => $rememberMe,
+                        );
+                        Pi::service('event')->trigger('user_login', $args);
+                        // Get user information
+                        $user = Pi::model('user_account')->find($uid)->toArray();
+                        // Set return array
+                        $return['message']    = __('You have logged in successfully');
+                        $return['login']      = 1;
+                        $return['identity']   = $user['identity'];
+                        $return['userid']     = $user['id'];
+                        $return['sessionid']  = Pi::service('session')->getId();
+                    } else {
+                        $return['message'] = __('Bind error');
+                    }
+                } else {
+                    $return['message'] = __('Invalid input please try again');
+                }
             }
         }
+        // json output
+        $this->view()->setTemplate(false)->setLayout('layout-content');
         echo Json::encode($return);
         exit;
     }
 
     public function logoutAction()
     {
+        // Get user id
         $uid = Pi::user()->getId();
+        // Logout user actions
         Pi::service('session')->manager()->destroy();
         Pi::service('user')->destroy();
         Pi::service('event')->trigger('logout', $uid);
-
+        // Set retrun array
         $return = array(
             'message'   => __('You logged out successfully.'),
             'logout'    => 1,
         );
+        // json output
         $this->view()->setTemplate(false)->setLayout('layout-content');
         echo Json::encode($return);
         exit;
@@ -72,28 +116,45 @@ class IndexController extends ActionController
 
     public function checkAction()
     {
-        $id = $this->params('id', '');
-
-        $return = array();
-
-        if (!empty($id)) {
-            Pi::service('session')->regenerateId($id);
-        }
-
-        if (Pi::service('user')->hasIdentity()) {
-            $return = array(
-                'check'     => 1,
-                'uid'       => Pi::user()->getId(),
-                'sessionid' => Pi::service('session')->getId(),
-            );
+        // Check user login from allowed or not
+        if ($this->config('active_login')) {
+            // Get session id
+            $id = $this->params('id', '');
+            // Check id set or not
+            if (!empty($id)) {
+                // Start session
+                $session = Pi::model('session')->find($id);
+                if ($session) {
+                    session_id($id);
+                    Pi::service('session')->manager()->start();
+                }
+            }
+            // Check user has identity
+            if (Pi::service('user')->hasIdentity()) {
+                $return = array(
+                    'check'     => 1,
+                    'uid'       => Pi::user()->getId(),
+                    'identity'  => Pi::user()->getIdentity(),
+                    'sessionid' => Pi::service('session')->getId(),
+                );
+            } else {
+                $return = array(
+                    'check'     => 0,
+                    'uid'       => Pi::user()->getId(),
+                    'identity'  => Pi::user()->getIdentity(),
+                    'sessionid' => Pi::service('session')->getId(),
+                );
+            }
         } else {
+            // Set empty return
             $return = array(
                 'check'     => 0,
                 'uid'       => 0,
-                'sessionid' => Pi::service('session')->getId(),
+                'identity'  => '',
+                'sessionid' => '',
             );
         }
-
+        // json output
         $this->view()->setTemplate(false)->setLayout('layout-content');
         echo Json::encode($return);
         exit;
